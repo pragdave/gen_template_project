301113e5d96a9cab5e3acfb36682d0a08060a8bf
pragdave <dave@pragdave.me>
2017-04-17 14:15:07 -0500

    unify option processing and get rid of populate_assigns

--- lib/project.ex
+++ lib/project.ex
@@ -5,7 +5,7 @@ defmodule Mix.Gen.Template.Project do
   use MixTemplates,
     name:       :project,
     short_desc: "Simple template for projects (with optional app and supervision)",
-    source_dir: "../template"
+    source_dir: "../template",
     options:    [
       sup:         [ to: :is_supervisor?,          default:  false ],
       supervisor:  [ same_as: :sup ],
