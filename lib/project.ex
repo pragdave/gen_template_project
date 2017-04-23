83db794ffe469e40f280bda8640da7072f080b6c
pragdave <dave@pragdave.me>
2017-04-23 14:53:35 -0500

    add option documentation

--- lib/project.ex
+++ lib/project.ex
@@ -1,18 +1,34 @@
 defmodule Mix.Gen.Template.Project do
 
   @moduledoc File.read!(Path.join([__DIR__, "../README.md"]))
-  
+
   use MixTemplates,
     name:       :project,
     short_desc: "Simple template for projects (with optional app and supervision)",
     source_dir: "../template",
     options:    [
-      sup:         [ to: :is_supervisor?,          default:  false ],
+      sup: [
+        to:       :is_supervisor?,          
+        default:  false,
+        desc:     "creates a top-level supervisor, and adds the app to mix.exs"
+      ],
       supervisor:  [ same_as: :sup ],
-      app:         [ to: :app,                     required: false ],
+
+      app: [
+        to:       :app,
+        required: false,
+        takes:    "app_name",
+        desc:     "sets the application name to «app_name»"
+      ],
       application: [ same_as: :app ],
-      module:      [ to: :project_name_camel_case, required: false ] 
+
+      module: [
+         to:        :project_name_camel_case,
+         required:  false,
+         takes:     "«project_name»",
+         desc:      "sets the name of the module to «project_name»"
+       ]
     ]
-  
-  
+
+
 end
